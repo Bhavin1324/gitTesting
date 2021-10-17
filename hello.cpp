6124bc761e0b914f16eee3ee953ed1117b95e2d0
bj <barberrionking@gmail.com>
2021-10-17 18:40:39 +0530

    Jaha the vaha aa gaye

---
+++ hello.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class base{
+    private:
+    int a,b;
+    public:
+    // int sumup(int,int);
+    // int avg(int,int);
+    int sumup(int a,int b){
+        return a+b;
+    }
+    int avg(int a,int b){
+    return (a+b)/2;
+} 
+};
+/* int base::sumup(int a,int b){
+    return a+b;
+}
+int base::avg(int a,int b){
+    return (a+b)/2;
+} */
+int main(){
+    base obj;
+    int a,b;
+    // Taking niput form usre 
+    cout<<"Enter first number:"<<endl;
+    cin>>a;
+    cout<<"Enter second number:"<<endl;
+    cin>>b;
+    //implementation of object
+    cout<<"Answer: " << obj.sumup(a,b);
+    cout<<"Average: " << obj.avg(a,b);
+    return 0;
+}
